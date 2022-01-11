2925209ba89373b1dd19a2645c8fe9198f8d297b
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2022-01-11 20:09:26 +0200

    fixed overlook in get_prevs: it would only start checking at the third tetriminos

--- handle_tetriminos.c
+++ handle_tetriminos.c
@@ -59,10 +59,13 @@ void	get_prevs(t_tet **tets)
 	while (tets[i])
 	{
 		j = i - 1;
-		while (j > 0)
+		while (j >= 0)
 		{
 			if (ft_memcmp(tets[j]->coords, tets[i]->coords, 32) == 0)
+			{
 				tets[i]->prev = tets[j];
+				break ;
+			}
 			if (j == 0)
 				break ;
 			j--;
